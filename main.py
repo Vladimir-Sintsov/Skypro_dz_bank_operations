c01c5d14c5508e5798034a3f3ae6f67dcf87ef6a
Владимир Синцов <hdd220w@mail.ru>
2025-06-20 22:26:04 +0300

    добавил исключения для flake8 исправил имена переменных

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from src.processing import filter_by_state
+from src.processing import transaction_list
+from src.processing import sort_by_date
 from src.widget import get_date
 from src.widget import mask_account_card
-from src.processing import filter_by_state, sort_by_date, operation_list
 
 print(mask_account_card("Maestro 1596837868705199"))
 print(mask_account_card("Счет 64686473678894779589"))
@@ -13,7 +15,7 @@ print(mask_account_card("счет 73654108430135874305"))
 
 print(get_date("2024-03-11T02:26:18.671407"))
 
-executed_operations = filter_by_state(operation_list)
+executed_operations = filter_by_state(transaction_list)
 sorted_operations = sort_by_date(executed_operations)
 
 for op in sorted_operations:
--- src/processing.py
+++ src/processing.py
@@ -1,22 +1,24 @@
-from typing import Any, Dict, List
+from typing import Any
+from typing import Dict
+from typing import List
 
-operation_list = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+transaction_list = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
 
-def filter_by_state(operation_list: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
+def filter_by_state(transaction: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """
     Возвращает новый список операций, отфильтрованный по значению ключа 'state'.
     """
-    return [item for item in operation_list if item.get('state') == state]
+    return [item for item in transaction if item.get("state") == state]
 
 
-def sort_by_date(operation_list: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+def sort_by_date(transaction: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
     """
     Сортирует список операций по дате (ключ 'date') в порядке от самой новой к самой старой.
     """
-    return sorted(operation_list, key=lambda item: item['date'], reverse=True)
+    return sorted(transaction, key=lambda item: item["date"], reverse=True)
