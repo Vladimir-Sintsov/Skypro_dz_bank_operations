0908a379c257b2799306330cff983997bb76e022
Владимир Синцов <hdd220w@mail.ru>
2025-08-12 18:10:29 +0300

    в модуль processing.py добавлены новые функции для поиска транзакций по искомым словам и получения словаря с категориями и их количеством.

--- src/processing.py
+++ src/processing.py
@@ -2,13 +2,7 @@ from datetime import datetime
 from typing import Any
 from typing import Dict
 from typing import List
-
-transaction_list = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
+import re
 
 
 def filter_by_state(transaction: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
@@ -36,3 +30,47 @@ def sort_by_date(transaction: List[Dict[str, Any]], reverse_list: bool = True) -
                 continue
 
     return sorted(valid_items, key=lambda items: datetime.fromisoformat(items["date"]), reverse=reverse_list)
+
+
+
+def get_searched_transactions(transactions: list, search_str: str) -> List[dict]:
+    """
+    Возвращает список транзакций, у которых в описании есть строка search_str (игнорируя регистр).
+    """
+    if not search_str:
+        return []
+
+    pattern = re.compile(re.escape(search_str), re.IGNORECASE)
+    result = []
+
+    for transaction in transactions:
+        if "description" in transaction:
+            if re.search(pattern, transaction["description"]):
+                result.append(transaction)
+        # Если ключа нет, пропускаем транзакцию
+
+    return result
+
+
+def get_transactions_count_by_category(transactions: list, category_list: list) -> dict:
+    """
+    Возвращает словарь с количеством транзакций по каждой категории.
+    """
+    if not category_list:
+        raise Exception("Список категорий пуст")
+
+    result = {}
+
+    for category in category_list:
+        pattern = re.compile(re.escape(category), flags=re.IGNORECASE)
+        counter = 0
+
+        for transaction in transactions:
+            if "description" in transaction:
+                if re.search(pattern, transaction["description"]):
+                    counter += 1
+
+        if counter > 0:
+            result[category] = counter
+
+    return result
